c0577a7c3ca8d497274078f47fa7151acf21deba
Roberto <robertoluna3018@gmail.com>
2022-05-06 12:11:30 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -117,7 +117,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
 		preOrderRecurse(root); 
-		 System.out.println("PreOrder test commit"); 
+		 System.out.println("PreOrder test commit");
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
@@ -138,7 +138,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
 		inOrderRecurse(root); 
-		 System.out.println(InOrder test commit);
+		 System.out.println("InOrder test commit");
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
@@ -155,11 +155,17 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		postOrderRecurse(root); 
-		 System.out.println(PostOrder test commit); 
+		 System.out.println("PostOrder test commit"); 
 	}
 	
 	public void postOrderRecurse(BSTNode<T> node) {
-		
+		if(node == null)
+		{
+			return;
+		}
+		postOrderRecurse(node.leftChild);
+		postOrderRecurse(node.rightChild);
+		System.out.print(node.data + " ");
 	}
 	
 	//Traverse the tree in an postorder fashion uses Stacks. 
@@ -170,15 +176,27 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		Stack<BSTNode<T>> post = new Stack<>();
 		Stack<BSTNode<T>> postHelper = new Stack<>();
 		if(root!=null) {
-			postHelper.push(root);
-			while(!postHelper.isEmpty()) {
-				//how should post and postHelper be updated?
-			}
+			post.push(root);
 			
 			while(!post.isEmpty()) {
 				BSTNode<T> node = post.pop();
-				System.out.print(node + " ");
+				postHelper.push(node);
+				
+				if(node.leftChild != null)
+				{
+					post.push(node.leftChild);
+				}
+				if(node.rightChild != null)
+				{
+					post.push(node.rightChild);
+				}
 			}
+			while(!postHelper.isEmpty()) {
+				//how should post and postHelper be updated?
+				BSTNode<T> node = postHelper.pop();
+				System.out.print(node.data + " ");
+			}
+			
 		}
 
 	}
